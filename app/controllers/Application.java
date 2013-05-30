66918c808bc52d89c91e83313961bf1f5b0faad4
Lydian Lee <lydian@cmu.edu>
2013-05-29 19:59:15 -0700

    add user interface

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,8 +7,9 @@ import views.html.*;
 
 public class Application extends Controller {
   
+   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(main.render());
     }
   
 }
