ba6108ddb9f53b7bd2ec03e298cff0a44e4f625e
lakeskysea <lakeskysea@gmail.com>
2013-06-04 16:55:58 -0700

    replace user object in client-server api

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -54,7 +54,7 @@ public class Application extends Controller {
     
     @SecureSocial.SecuredAction
     public static Result userFeed() throws JsonGenerationException, JsonMappingException, IOException {
-    	User user = new User();
+    	Identity user = (Identity) ctx().args.get(SecureSocial.USER_KEY);
     	return ok(new ObjectMapper().writeValueAsString(user));
     }
 }
