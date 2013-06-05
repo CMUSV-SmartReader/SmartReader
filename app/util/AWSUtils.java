3c80b6833ea9fb6471269ebab161949fcbda4b2b
Lydian Lee <lydian@cmu.edu>
2013-06-05 14:13:13 -0700

    start and stop aws instace

--- app/util/AWSUtils.java
+++ app/util/AWSUtils.java
@@ -1,32 +1,65 @@
 package util;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import com.amazonaws.auth.AWSCredentials;
+import play.Logger;
+
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ec2.model.StartInstancesRequest;
+import com.amazonaws.services.ec2.model.StartInstancesResult;
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+import com.amazonaws.services.ec2.model.StopInstancesResult;
 
 public class AWSUtils {
-	private AmazonEC2Client ec2;
-	private AWSCredentials credentials;
-
-	public AWSUtils() {
-		AWSCredentialsProvider credentialsProvider = new EnvironmentVariableCredentialsProvider() ;
-		
-		ec2 = new AmazonEC2Client(credentialsProvider);
-		DescribeInstancesResult describeInstanceRequest = ec2.describeInstances();
-		List<Reservation> reservations = describeInstanceRequest.getReservations();
-		System.out.println(reservations.toString());
-		//System.out.println(new StartInstancesRequest().getInstanceIds().toString());
-		
-		//StartInstancesRequest startRequest = new StartInstancesRequest().withInstanceIds("i-a4fef7fc");
-		 //client.startInstances(startRequest);
-		 
-
+    private AmazonEC2Client ec2;
+    public AWSUtils() throws InterruptedException {
+        AWSCredentialsProvider credentialsProvider = new EnvironmentVariableCredentialsProvider() ;
+        ec2 = new AmazonEC2Client(credentialsProvider);
+        ec2.setEndpoint("ec2.us-west-1.amazonaws.com");
 	}
+    public void startInstance(String instanceId) throws InterruptedException{
+        StartInstancesRequest startRequest = new StartInstancesRequest().withInstanceIds(instanceId);
+        StartInstancesResult result = ec2.startInstances(startRequest);
+        Logger.debug("Started instance.  State is " + result.getStartingInstances().get(0).getCurrentState());
+        while(true){
+            if(!getInstanceStatus(instanceId).equals("pending")) break;
+            Thread.sleep(1000);
+        }
+    }
+    public String getInstanceStatus(String instanceId){
+        DescribeInstancesRequest instanceRequest = new DescribeInstancesRequest().withInstanceIds(instanceId);
+        Instance instance = ec2.describeInstances(instanceRequest).getReservations().get(0).getInstances().get(0);
+        return instance.getState().getName();
+    }
+    
+    public void stopInstance(String instanceId) throws InterruptedException{
+        StopInstancesRequest stopInstancesRequest = new StopInstancesRequest().withInstanceIds(instanceId);
+        StopInstancesResult result = ec2.stopInstances(stopInstancesRequest);
+        Logger.debug("Stopping instance.  State is " + result.getStoppingInstances().get(0).getCurrentState());
+        while(true){
+            if(!getInstanceStatus(instanceId).equals("stopping")) break;
+            Thread.sleep(1000);
+        }
+    }
+    public void printStatus(){
+        System.out.println("You have access to " + ec2.describeAvailabilityZones().getAvailabilityZones().size() + " Availability Zones.");
+        
+        DescribeInstancesResult describeInstancesRequest = ec2.describeInstances();
+        List<Reservation> reservations = describeInstancesRequest.getReservations();
+        Set<Instance> instances = new HashSet<Instance>();
+
+        for (Reservation reservation : reservations) {
+            instances.addAll(reservation.getInstances());
+        }
+        System.out.println("You have " + instances.size() + " Amazon EC2 instance(s) running.");
+
+    }
 }
--- test/util/AWSUtilsTest.java
+++ test/util/AWSUtilsTest.java
@@ -1,16 +1,24 @@
 package util;
 
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class AWSUtilsTest {
-
-	@Test
-	public void testConnection() {
-		System.out.println(System.getenv("AWS_ACCESS_KEY_ID"));
-		//AWSUtils au = new AWSUtils();
-		
-	}
-
+    private AWSUtils aws;
+    private String instanceId = "i-a4fef7fc";
+    
+    @Before
+    public void init() throws InterruptedException {
+        aws = new AWSUtils();
+    }
+    
+    @Test
+    public void testConnection() throws InterruptedException {
+        System.out.println(aws.getInstanceStatus(instanceId));
+        aws.startInstance(instanceId);
+        System.out.println(aws.getInstanceStatus(instanceId));
+        aws.stopInstance(instanceId);
+        System.out.println(aws.getInstanceStatus(instanceId));
+        
+    }
 }
