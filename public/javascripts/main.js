7e160a304ad223bd78df2153d27617fe658a8a9a
clydeli <clyde1008li@gmail.com>
2013-06-05 18:52:06 -0700

    add some dummy data

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,8 +8,9 @@ $(window).on('load', function(){
 //  ]);
 
   smartreader.loadCategories();
+  smartreader.putTitle("Home");
 
-  /*$('a').on('click', ''){
+  /*$('li').on('click', ''){
     $.ajax({
       dataType: "json",
       url: "",
@@ -21,10 +22,10 @@ $(window).on('load', function(){
   };*/
 
   // Put dummy articles
-  /*smartreader.putTitle("Technology");
+  //smartreader.putTitle("Technology");
   smartreader.putArticles([
-    new smartreader.article( "Title1", "Author1", (new Date()), "Feed1", "Description 1" ),
-    new smartreader.article( "Title2", "Author2", (new Date()), "Feed2", "Description 2" ),
-    new smartreader.article( "Title3", "Author3", (new Date()), "Feed3", "Description 3" ),
-  ]);*/
+    new smartreader.article( "Humble Mumble", "Humble Mumble", "Monday", "Humble Bundle", "Humble Indie Bundle 8 is now 11 games strong! We just added four stupendous games to the Humble Indie Bundle 8! If you’ve already purchased a bundle, you’ll find *Tiny & Big in Grandpa’s Leftovers*, *English Country Tune*, *Intrusion 2*, and *Oil Rush* available now on your download page! These ... See more from software" ),
+    new smartreader.article( "Making Google’s CalDAV and CardDAV APIs available for everyone", "Scott Knaster", "9:15AM", "Google Code Blog", "In March we announced that CalDAV, an open standard for accessing calendar data across the web, would become a partner-only API because it appeared that almost all the API usage was driven by a few large developers. Since that announcement, we received many requests for access to CalDAV, giving us a better understanding of developers’ use cases and causing us to revisit that decision. In response " ),
+    new smartreader.article( "Morphing Devices", "Mary Lou", "May 15", "Feed3", "Today we want to share an experimental “morphing” slideshow with you. The idea is to transition between different devices that show a screenshot of a responsive website or app by applying a “device class”. By using the same elements and pseudo-elements for all the devices, we can create an interesting morph effect. We will control the classes and the switching of the image with some Java" ),
+  ]);
 });
