4b078acb503f519822bca5620651376b0d938f71
clydeli <clyde1008li@gmail.com>
2013-06-18 00:03:52 -0700

    updated angularjs architecture

--- public/javascripts/thermoreader-controllers.js
+++ public/javascripts/thermoreader-controllers.js
@@ -1,15 +1,17 @@
 var thermoreader = thermoreader || {};
 
 thermoreader.mainCtrl = function($scope, dbFactory) {
-  $scope.allFeeds = dbFactory.allFeeds;
+
+  $scope.allFeeds = dbFactory.fetchAllFeeds(function(){
+    $scope.allFeeds = dbFactory.getAllFeeds();
+  });
+
   $scope.selectedFeed = {};
 
   $scope.selectFeed = function(feed){
     console.log(feed.id);
-    $scope.selectedFeed = { title: feed.title, articles: []};
-    dbFactory.getFeed(feed.id).success(function(d){
-      console.log(d);
-      $scope.selectedFeed = { title: d.title, articles: []};
+    dbFactory.getFeed(feed.id, function(feed){
+      $scope.selectedFeed = feed;
     });
   };
 
