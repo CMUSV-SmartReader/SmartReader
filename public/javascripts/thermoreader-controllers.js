bdbf36c17abc3699845ce1d6120401aff086053b
clydeli <clyde1008li@gmail.com>
2013-06-19 19:58:19 -0700

    order by date or popularity

--- public/javascripts/thermoreader-controllers.js
+++ public/javascripts/thermoreader-controllers.js
@@ -2,38 +2,38 @@ var thermoreader = thermoreader || {};
 
 thermoreader.mainCtrl = function($scope, dbFactory) {
 
+  $scope.orderRule =
+    localStorage.hasOwnProperty('orderRule')? localStorage['orderRule']:"popular";
+
   $scope.allFeeds = dbFactory.getAllFeeds(function(allFeeds){
     $scope.allFeeds = allFeeds;
   });
 
   $scope.selectedFeed = dbFactory.getRecommendations(function(recommendations) {
     $scope.selectedFeed = { name: "Recommendations", articles: recommendations };
-    // $scope.selectedFeed.articles.sort(function(a, b){
-    //   return (a.popular>b.popular);
-    // });
   });
 
   $scope.selectFeed = function(feed) {
     $scope.selectedFeed = dbFactory.getFeed(feed.id, function(feed){
       $scope.selectedFeed = feed;
-      // $scope.selectedFeed.article = $scope.selectedFeed.articles.sort(function(a, b){
-      //   return (a.popular>b.popular);
-      // });
     });
   };
 
   $scope.getRecommendations = function() {
     $scope.selectedFeed = dbFactory.getRecommendations(function(recommendations){
       $scope.selectedFeed = { name: "Recommendations", articles: recommendations };
-      // $scope.selectedFeed.article = $scope.selectedFeed.articles.sort(function(a, b){
-      //   return (a.popular>b.popular);
-      // });
     });
   };
 
   $scope.expandArticle = function(article){
     article.expanded = !article.expanded;
     article.read = true;
+  };
+
+
+  $scope.setOrderRule = function(rule){
+    $scope.orderRule = rule;
+    localStorage['orderRule'] = rule;
   };
 
   $scope.$on('$viewContentLoaded', function(){
