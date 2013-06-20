628486e1894d0296cf42852ebeb3b55c65b027c4
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2013-06-19 17:48:06 -0700

    Change Structure

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,14 @@ var thermoreader = thermoreader || {};
 
 thermoreader.instance = angular.module('thermoReader', [])
   .factory('dbFactory', thermoreader.db)
-  .controller('mainCtrl', thermoreader.mainCtrl);
+  .controller('mainCtrl', thermoreader.mainCtrl)
+  .config(function($routeProvider) {
+  	$routeProvider
+  	.when("/", {controller: "mainCtrl", templateUrl: "/assets/templates/main.html"})
+  	.when("/manage", {controller: "manageCtrl", templateUrl: "/assets/templates/manage.html"})
+  }
+);
+
 
 
 $(document).on('ready', function(){
--- public/javascripts/thermoreader-controllers.js
+++ public/javascripts/thermoreader-controllers.js
@@ -6,21 +6,25 @@ thermoreader.mainCtrl = function($scope, dbFactory) {
     $scope.allFeeds = allFeeds;
   });
 
-  $scope.selectedFeed = dbFactory.getRecommendations(function(recommendations){
+  $scope.selectedFeed = dbFactory.getRecommendations(function(recommendations) {
     $scope.selectedFeed = { name: "Recommendations", articles: recommendations };
   });;
 
-  $scope.selectFeed = function(feed){
+  $scope.selectFeed = function(feed) {
     $scope.selectedFeed = dbFactory.getFeed(feed.id, function(feed){
       $scope.selectedFeed = feed;
     });
   };
 
-  $scope.getRecommendations = function(){
+  $scope.getRecommendations = function() {
     $scope.selectedFeed = dbFactory.getRecommendations(function(recommendations){
       $scope.selectedFeed = { name: "Recommendations", articles: recommendations };
     });
   }
 
-}
+};
+
+thermoreader.manageCtrl = function($scope, dbFactory) {
+
+};
 
