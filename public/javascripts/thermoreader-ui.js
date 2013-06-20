5270287a812bc85ed9550e42f09a24e99c16af2e
clydeli <clyde1008li@gmail.com>
2013-06-19 18:59:07 -0700

    remove old onload bindings

--- public/javascripts/thermoreader-ui.js
+++ public/javascripts/thermoreader-ui.js
@@ -7,22 +7,10 @@ thermoreader.ui = (function(){
       $('#menu-toggle-btn').on('click', function(){
         $('#side-container').toggleClass('span2 span1 hidden');
       });
-
-      $('#content-container').on('click', '.continue-article', function(){
-        if($(this).html() != 'Less...'){
-          $(this).html('Less...');
-        }
-        else {
-          $(this).html('More...');
-        }
-      });
-
     };
 
   return {
     init : function(){
-      $('#side-container').perfectScrollbar({wheelSpeed: 60});
-      $('#content-container').perfectScrollbar({wheelSpeed: 60});
       binding();
     }
   };
