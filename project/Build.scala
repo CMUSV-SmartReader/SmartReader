b0f20332e00e6d4eaf5298c49b8d20c41a0758ef
Lydian Lee <lydian@cmu.edu>
2013-07-09 04:17:42 -0700

    fix env

--- project/Build.scala
+++ project/Build.scala
@@ -37,19 +37,7 @@ object ApplicationBuild extends Build {
    playStage <<= (playStage, baseDirectory) map {(stageCommand, baseDir) =>
       val content = """#!/usr/bin/env sh
                       |
-                      |# for ThermoReader
-                      |# for reader
-                      |export GITHUB_CLIENT_ID=dca1eb286de0808b8c1f
-                      |export GITHUB_CLIENT_SECRET=17233b7b09b826773b12a5a3903a2693e1e7affa
-                      |export FACEBOOK_CLIENT_ID=478707512205435
-                      |export FACEBOOK_CLIENT_SECRET=bb4fc429713005e4c1d1378108a34d49
-                      |export GOOGLE_CLIENT_ID=94895237141-lu9kgt0298h5c8u63kkqv3ejoakkulp0.apps.googleusercontent.com
-                      |export GOOGLE_CLIENT_SECRET=peNdMDBz3VLcGJPb6B2Ae1Xa
-                      |export AWS_ACCESS_KEY_ID=AKIAJEYU3OVITOZQJB7Q
-                      |export AWS_SECRET_KEY=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
-                      |export READER_DB_NAME=thermoreader
-                      |
-                      |exec sudo READER_DB_NAME=thermoreader FACEBOOK_CLIENT_ID=478707512205435 FACEBOOK_CLIENT_SECRET=bb4fc429713005e4c1d1378108a34d49 GOOGLE_CLIENT_ID=94895237141-lu9kgt0298h5c8u63kkqv3ejoakkulp0.apps.googleusercontent.com GOOGLE_CLIENT_SECRET=peNdMDBz3VLcGJPb6B2Ae1Xa java $@ -cp "`dirname $0`/staged/*" play.core.server.NettyServer `dirname $0`/..""".stripMargin
+                      |exec sudo READER_DB_HOST=test.lydian.tw READER_DB_NAME=thermoreader FACEBOOK_CLIENT_ID=478707512205435 FACEBOOK_CLIENT_SECRET=bb4fc429713005e4c1d1378108a34d49 GOOGLE_CLIENT_ID=94895237141-lu9kgt0298h5c8u63kkqv3ejoakkulp0.apps.googleusercontent.com GOOGLE_CLIENT_SECRET=peNdMDBz3VLcGJPb6B2Ae1Xa java $@ -cp "`dirname $0`/staged/*" play.core.server.NettyServer `dirname $0`/..""".stripMargin
       IO.write(baseDir / "target" / "start", content)
       stageCommand
     }
