de5b22363d1ed49c51f19f40f298747466f73653
lakeskysea <lakeskysea@gmail.com>
2013-07-22 10:38:06 -0700

    clearer uploaded file structure

--- app/controllers/FeedImportController.java
+++ app/controllers/FeedImportController.java
@@ -3,12 +3,20 @@ package controllers;
 import java.io.File;
 
 import play.mvc.Controller;
+import play.mvc.Http.RawBuffer;
 import play.mvc.Result;
+import play.mvc.Http.RequestBody;
 
 public class FeedImportController extends Controller{
 
     public static Result upload() {
-        File file = request().body().asRaw().asFile();
+        RequestBody body = request().body();
+        if (body != null) {
+            RawBuffer rawBuffer = body.asRaw();
+            if (rawBuffer != null) {
+                File uploadedFile = rawBuffer.asFile();
+            }
+        }
         return ok("||||||||||||||||||||||||| upload triggered");
       }
 }
